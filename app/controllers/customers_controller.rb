d9fcc1228282fdc951ff187093635df76fc14e6d
CoffeeAndCream <seanjhulse@gmail.com>
2017-05-15 15:43:39 -0500

    Integrated calendar with leads

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,6 @@
 class CustomersController < ApplicationController
   helper_method :sort_column, :sort_direction
-  has_scope :zip, :user, :city, :fullname, :address, :city, :customer
+  has_scope :zip, :user, :city, :fullname, :first_name, :last_name, :address, :city, :customer
   def index
     @customers = apply_scopes(Lead).where(:customer => true).search(params[:search]).order(sort_column + " " + sort_direction).paginate(page: params[:page], per_page: 25)
   end
