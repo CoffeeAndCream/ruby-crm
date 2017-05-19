aacb210c199366c78035a72a8038eae44c302d38
CoffeeAndCream <seanjhulse@gmail.com>
2017-05-19 14:57:16 -0500

    Search form updated

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -23,10 +23,10 @@ class LeadsController < ApplicationController
     @user = User.find_by_id(@lead.user_id)
 
     unless @lead.customer
+      @lead.update_attributes(customer: true)
       if @user.nil?
         redirect_to :action => 'show', :id => @lead
       else
-        @lead.update_attributes(customer: true)
         UserMailer.lead_email(@user, @lead).deliver_now
         redirect_to :action => 'show', :id => @lead
       end
