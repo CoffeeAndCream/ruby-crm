0cbf994bd60b7cbbde79b416c5c28134a543421c
CoffeeAndCream <seanjhulse@gmail.com>
2017-06-22 10:55:43 -0500

    Added a finished project option

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ApplicationRecord
   has_one :lead, inverse_of: :project
   has_many :tasks, dependent: :destroy
   accepts_nested_attributes_for :tasks
+  scope :finished, -> finished { where(:finished => finished) }
 
   attr_accessor :name, :details, :completed, :salesperson
 
