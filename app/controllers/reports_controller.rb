8c97ac81ea2c1072e376fa759a6601dfc5225e0d
CoffeeAndCream <seanjhulse@gmail.com>
2017-07-26 08:48:23 -0500

    Leads converted to Customers Chart in Reports added

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,12 +1,13 @@
 class ReportsController < ApplicationController
   before_action :authenticate_active_admin_user!
   def index
-    @data = Lead.group_by_week(:created_at, last: 12).count
+    @data = Lead.group_by_week(:created_at, last: 6).count
   end
   def create
     begin_date = params[:dates][:begin_date]
     end_date = params[:dates][:end_date]
-    @data = Lead.where('created_at BETWEEN ? AND ?', Date.parse(begin_date), Date.parse(end_date)).group_by_week(:created_at, last: 12).count
+    weeks = ((Date.parse(end_date) - Date.parse(begin_date)).to_i/7)
+    @data = Lead.where('created_at BETWEEN ? AND ?', Date.parse(begin_date), Date.parse(end_date)).group_by_week(:created_at, last: weeks).count
     unless @data.nil? || @data.empty?
       respond_to do |format|
         format.js { render 'date_update.js.erb'}
